c6daa9497226414dcf6e859f3b7ec4a26187aebe
VinaySG76 <sgvinay76.co@gmail.com>
2025-09-10 00:27:12 +0530

    Create outputs.tf

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.user_files.bucket
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.endpoint
+}
